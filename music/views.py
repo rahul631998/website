81a3ac72708db40b2fc44c1748a44588f108bd5b
rahul631998 <rahulchaudhary631998@gmail.com>
2018-06-08 22:20:04 +0530

    after adding index.html file

--- music/views.py
+++ music/views.py
@@ -1,14 +1,14 @@
 from django.http import HttpResponse
-from boto.connection import HTTPResponse
+from django.template import loader
 from .models import Album
 def index(request):
     all_albums = Album.objects.all()
-    html = ''
-    for album in all_albums:
-        url = '/music/' + str(album.id) + '/'
-        html += '<a href = "' + url + '">' + album.album_title + '</a><br>'
+    template = loader.get_template('music/index.html')
+    context = {
+                'all_albums' : all_albums
+        }
     
-    return HttpResponse(html)
+    return HttpResponse(template.render(context, request))
 
 def detail(request, album_id):
     return HttpResponse("<h2> details of album_id "+str(album_id)+ " </h2>") 
